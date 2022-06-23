943772ac2d04e7913c717d787d068310a9d7f48c
Matthew Davis <matthewdavis1372@yahoo.com>
2022-06-23 00:00:32 -0400

    just updating

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const Manager = require("./lib/manager")
 const Engineer = require("./lib/engineer")
 const Intern = require("./lib/intern")
 const Employee = require("./lib/employee")
+const fs = require("fs")
+const inquirer = require('inquirer')
+
+// team manager’s name, employee ID, email address, and office number
+
+
+const promptUser = () => {
+    
+}
+
+
